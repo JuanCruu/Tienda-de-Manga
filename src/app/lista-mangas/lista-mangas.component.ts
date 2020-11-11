fdfb34474201475847d1c919f06c9885a193e3a2
JuanCruu <juancruznavarro592@gmail.com>
2020-11-11 02:29:57 -0300

    se agregaron los primeros componentes

--- src/app/lista-mangas/lista-mangas.component.ts
+++ src/app/lista-mangas/lista-mangas.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaMangasComponent implements OnInit {
 
+  manga={
+    id:1,
+    nombre:"Naruto",
+    descripcion:"Es naruto man",
+    genero:[" ","Shonen","Yaoi","Ninjas"],
+    precio:60.55,
+    paginas:["https://otakuteca.com/images/books/cover/5df322602c2bb.jpg","urlPagina2","urlPagina3","urlPagina4","urlPagina5","urlPagina6","urlPagina7","urlPagina8","urlPagina9","urlPagina10","urlPagina11"],
+    comprado:false
+  }
   constructor() { }
 
   ngOnInit(): void {
